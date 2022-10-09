e699c1b7ef943ea5c1ae490a457bdf000fbaa4af
marsanemar <111205473+marsanemar@users.noreply.github.com>
2022-10-08 22:04:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ const getWeb3 = () => {
   })
 }
 
-const contractAddress = '0x072d29B6691B0b8e79B1Be9F865745C4BFC96307'
+const contractAddress = '0x171c44a95BF1BC8A867579c023602c676e75843B'
 const abi = JSON.parse(
-  '[ { "inputs": [], "name": "execute", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "stake", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "exampleExternalContractAddress",	"type": "address" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address",	"name": "sender",	"type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Stake", "type": "event" }, { "inputs": [], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "balance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "balances", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "deadline", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "exampleExternalContract", "outputs": [ { "internalType": "contract ExampleExternalContract", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "threshold", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "timeLeft", "outputs": [ { "internalType": "uint256", "name": "timeleft", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ]'
+  '[ { "inputs": [ { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "uint256", "name": "amountPerHero", "type": "uint256" }, { "internalType": "string", "name": "description", "type": "string" } ], "name": "stake", "outputs": [], "stateMutability": "payable", "type": "function" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "sender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Stake", "type": "event" }, { "inputs": [], "name": "withdraw", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "activeMission", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getHeroBalance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getMissionBalance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, {	"inputs": [],	"name": "getMissionDescription", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getMissionIncentivePerHero", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "heroBalances", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "missionBalances", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "missionDetail", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "",	"type": "address" } ], "name": "missionIncentivePerHero", "outputs": [ {	"internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ]'
 )
+
+
 
 
 
@@ -34,19 +36,53 @@ const getContract = async web3 => {
 
 //Smart contract functions
 const balance = async (result, contract, accounts) => {
-  result = await contract.methods.balance().call({ from: accounts[0] })
-  document.getElementById('lastInfo').innerHTML = result
+  result = await contract.methods.getMissionBalance().call({ from: accounts[0] })
+  document.getElementById('infoMissionBalance').innerHTML = result
 }
 
+const balance2 = async (result, contract, accounts) => {
+  result = await contract.methods.getMissionBalance().call({ from: accounts[0] })
+  document.getElementById('infoMissionBalance2').innerHTML = result
+}
+
+//Smart contract functions
+const heroBalance = async (result, contract, accounts) => {
+  result = await contract.methods.getHeroBalance().call({ from: accounts[0] })
+  document.getElementById('infoHeroBalance').innerHTML = result
+}
+
+
+//Smart contract functions
+const missionDescription = async (result, contract, accounts) => {
+  result = await contract.methods.getMissionDescription().call({ from: accounts[0] })
+  document.getElementById('infoMissionDescription').innerHTML = result
+}
+
+//Smart contract functions
+const rewardHero = async (result, contract, accounts) => {
+  result = await contract.methods.getMissionIncentivePerHero().call({ from: accounts[0] })
+  document.getElementById('infoRewardPerHero').innerHTML = result
+}
+
+
+
 const stake = (result, contract, accounts) => {
-  let input
-  $('#input').on('change', e => {
-    input = e.target.value
+  let inputDescription
+  let inputReward
+  let inputBudget
+  $('#inputDescription').on('change', e => {
+    inputDescription = e.target.value
+  })
+  $('#inputReward').on('change', e => {
+    inputReward = e.target.value
+  })
+  $('#inputBudget').on('change', e => {
+    inputBudget = e.target.value
   })
 
   $('#form').on('submit', async e => {
     e.preventDefault()
-    await contract.methods.stake(input).send({ from: accounts[0] })
+    await contract.methods.stake(inputBudget, inputReward, inputDescription).send({ from: accounts[0] })
     balance(result, contract, accounts)
   })
 }
@@ -75,6 +111,11 @@ async function quoteApp() {
   let quote
   
   balance(quote, contract,accounts)
+  balance2(quote, contract,accounts)
+  heroBalance(quote, contract,accounts)
+  missionDescription(quote, contract,accounts)
+  rewardHero(quote, contract,accounts)
+  
   withdraw(quote, contract, accounts)
   stake(quote, contract, accounts)
 }
