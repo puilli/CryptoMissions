10c80ceb268430971236758f0f5727a413244aca
marsanemar <111205473+marsanemar@users.noreply.github.com>
2022-10-08 21:02:41 -0500

    Contracts updated

--- contracts/Staker.sol
+++ contracts/Staker.sol
@@ -2,67 +2,49 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-import "./ExampleExternalContract.sol";
 
 contract Staker {
 
-  mapping(address => uint256) public balances;
-  uint256 public constant threshold = 1 ether;
-  uint256 public deadline = block.timestamp + 3000 seconds;
-
-  ExampleExternalContract public exampleExternalContract;
+  mapping(address => uint256) public missionBalances;
+  mapping(address => string) public missionDetail;
+  mapping(address => uint256) public missionIncentivePerHero;
+  mapping(address => uint256) public heroBalances;
+  address public activeMission;
 
   event Stake(address indexed sender, uint256 amount);
-
-  modifier deadlineReached( bool requireReached ) {
-    uint256 timeRemaining = timeLeft();
-    if( requireReached ) {
-      require(timeRemaining == 0, "Deadline not reached yet");
-    } else {
-      require(timeRemaining > 0, "Deadline is already reached");
-    }
-    _;
-  }
-
-  modifier stakeNotCompleted() {
-    bool completed = exampleExternalContract.completed();
-    require(!completed, "Staking process has ben already completed");
-    _;
-  }
+    
   
-  constructor(address exampleExternalContractAddress) {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  function stake(uint amount, uint amountPerHero, string memory description) public payable {
+    missionBalances[msg.sender] += amount;
+    missionIncentivePerHero[msg.sender] = amountPerHero;
+    missionDetail[msg.sender] = description;
+    emit Stake(msg.sender, amount);
   }
 
-  function execute() public stakeNotCompleted deadlineReached(false) {
-    uint256 contractBalance = address(this).balance;
-    require(contractBalance >= threshold, "Threshold not reached");
-
-   (bool sent,) = address(exampleExternalContract).call{value: contractBalance}(abi.encodeWithSignature("complete()"));
-    require(sent, "exampleExternalContract.complete failed");
+  function withdraw() public {
+    uint256 missionBalance = missionBalances[activeMission];
+    require(missionBalance > missionIncentivePerHero[activeMission], "Sorry the mission has ben Completed");
+    missionBalances[activeMission] = missionBalances[activeMission] - missionIncentivePerHero[activeMission];
+    heroBalances[msg.sender] = heroBalances[msg.sender] + missionIncentivePerHero[activeMission];
   }
 
-  function stake() public payable deadlineReached(false) stakeNotCompleted {
-    balances[msg.sender] += msg.value;
-    emit Stake(msg.sender, msg.value);
+  
+  function getMissionBalance() public view returns (uint){
+      return missionBalances[activeMission];
   }
 
-  function withdraw() public deadlineReached(true) stakeNotCompleted {
-    uint256 userBalance = balances[msg.sender];
-
-    require(userBalance > 0, "You don't have balance to withdraw");
-
-    balances[msg.sender] = 0;
-    (bool sent,) = msg.sender.call{value: userBalance}("");
-    require(sent, "Failed to send user balance back to the user");
+  function getMissionIncentivePerHero() public view returns (uint){
+      return missionIncentivePerHero[activeMission];
   }
 
-  function timeLeft() public view returns (uint256 timeleft) {
-    if( block.timestamp >= deadline ) {
-      return 0;
-    } else {
-      return deadline - block.timestamp;
-    }
+  function getMissionDescription() public view returns (string memory){
+      return missionDetail[activeMission];
   }
+
+  function getHeroBalance() public view returns (uint){
+      return heroBalances[msg.sender];
+  }
+
+
 
 }
